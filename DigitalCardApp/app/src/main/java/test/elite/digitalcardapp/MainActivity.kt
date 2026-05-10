51f54304f168fe561dfe2f46c9db48b089d20888
avodemhka <aelitaalmaz@gmail.com>
2026-05-10 15:26:55 +0300

    Add DigitalCardApp

---
+++ DigitalCardApp/app/src/main/java/test/elite/digitalcardapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package test.elite.digitalcardapp
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val btnContact = findViewById<TextView>(R.id.btnContact)
+
+        btnContact.setOnClickListener {
+
+            Toast.makeText(
+                this,
+                "İletişime Geçildi!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
